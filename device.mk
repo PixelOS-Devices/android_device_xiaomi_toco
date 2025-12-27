de2aedd008d2713eb0bad5ff0193748659b0a475
basamaryan <basam.aryan@gmail.com>
2025-12-27 07:16:46 +0100

    toco: Inherit device/oneplus/dolby
    Change-Id: I26fae5c3f0ab62445e628257943d7b6a48a2a478

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
+# Inherit from dolby
+$(call inherit-product-if-exists, device/oneplus/dolby/device.mk)
+
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-toco/device.mk)
 
@@ -26,6 +29,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_f4_36_02_0b_fhd_cmd_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_f4_36_02_0b_fhd_cmd_dsi_panel.xml \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_f4_41_06_0a_fhd_cmd_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_f4_41_06_0a_fhd_cmd_dsi_panel.xml \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_f4_42_06_0c_fhd_cmd_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_f4_42_06_0c_fhd_cmd_dsi_panel.xml
+
+# Dolby
+TARGET_USES_DOLBY := true
 
 # Fingerprint
 PRODUCT_PACKAGES += \
